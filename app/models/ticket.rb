9730eef4b6d60bd3c5720271d2f1cf7ca02911af
Moriai Haruto <h-moriai@esm.co.jp>
2012-08-24 21:12:40 +0900

    first commit

---
+++ app/models/ticket.rb
@@ -0,0 +1,5 @@
+class Ticket < ActiveRecord::Base
+
+ attr_accessible :address, :email_address, :name, :price_paid, :seat_id_seq
+ validates :name, presence: true
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tickets::Application.config.secret_token = 'cd83271f6fb154053a6663edef56a40878e65ee5f66dc837011f184bb99e7e4a1db1added930efc285977c0172213d5ca7c577e8f713916f3c02dc92e1cf1432'
